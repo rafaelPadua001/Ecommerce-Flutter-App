ff927b83652b7201741fce33073fd0232aa9532a
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-04-19 20:04:24 -0300

    Feat
    Create Card to listTile.
    Create inkwell on click card.

--- lib/Widgets/Subcategories/subcategories.dart
+++ lib/Widgets/Subcategories/subcategories.dart
@@ -14,37 +14,56 @@ class SubcategoryWidget extends StatelessWidget {
         title: Text('Subcategories'),
       ),
       body: FutureBuilder(
-        future: Subcategory().fetchSubcategories(categoryId: categoryId),
-        builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (snapshot.data != null) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                if (snapshot.data != null && index < snapshot.data!.length) {
-                  final subcategoryData = snapshot.data![index];
-                  return  ListTile(
-                      title: Text(subcategoryData['name']),
+          future: Subcategory().fetchSubcategories(categoryId: categoryId),
+          builder:
+              (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (snapshot.data != null) {
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                 crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                padding: const EdgeInsets.all(20),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  if (snapshot.data != null && index < snapshot.data!.length) {
+                    final subcategoryData = snapshot.data![index];
+                    return Card(
+                      child: InkWell(
+                        onTap: () => {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content:
+                                  Text('Clicou no ${subcategoryData['name']}')))
+
+                          // print('Clicou no ${subcategoryData['name']}'),
+                        },
+                        child: Column(children: [
+                          ListTile(
+                            title: Text(subcategoryData['name']),
+                            subtitle: Text(subcategoryData['name']),
+                          ),
+                        ]),
+                      ),
                     );
-                  
-                } else {
-                  // Se os dados forem nulos ou o índice estiver fora do intervalo, retorne um widget vazio
-                  return SizedBox();
-                }
-              },
-            );
-          } else {
-            return Text('Nenhum dado de categorias disponível.');
-          }
-        }),
+                  } else {
+                    // Se os dados forem nulos ou o índice estiver fora do intervalo, retorne um widget vazio
+                    return SizedBox();
+                  }
+                },
+              );
+            } else {
+              return Text('Nenhum dado de categorias disponível.');
+            }
+          }),
     );
-    
   }
 }
