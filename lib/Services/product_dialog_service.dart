6fd32ecf9be7668f2716c1b525f0d0c2fc8d00aa
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-04-22 21:51:04 -0300

    Feat
    Create dialog on click product.

---
+++ lib/Services/product_dialog_service.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ProductDialogService {
+
+  Future<List<Map<String, dynamic>>> fetchProductDialog({required String productId}) async {
+    final response = await http.get(Uri.parse('http://192.168.122.1:8000/api/product/${productId}'));
+
+    if(response.statusCode == 200){
+      Map<String, dynamic> data = json.decode(response.body);
+      return [data];
+    }
+    else{
+      throw Exception('Nenhum produto encontrado');
+    }
+  }
+}
