dd882a9aaca6be9ff53893a799637a6b8db9b534
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-05-09 22:17:29 -0300

    Fix
    Fix Store function and getCarts.

--- lib/Services/cart_service.dart
+++ lib/Services/cart_service.dart
@@ -26,53 +26,41 @@ class CartService {
       throw Exception('Nenhum produto encontrado');
     }
   }
-
-  Future<List<Map<String, dynamic>>> getCarts() async {
-    try {
+  Future <List<Map<String, dynamic>>> getCarts() async {
+    try{
       final _authUser = await _authService.getCurrentUser();
       if (_authUser == null) {
         throw Exception('Usuário não autenticado');
       }
 
-      final databaseReference = FirebaseDatabase.instance.ref();
+      final databaseReference = await getDatabase();
       DatabaseEvent event =
           await databaseReference.child('cart').child(_authUser.uid).once();
       DataSnapshot snapshot = event.snapshot;
+      List<Map<String, dynamic>> cart = []; 
 
-      if (!snapshot.exists) {
-        return [];
-      }
-
-      if (snapshot.value is List) {
-        try {
-          List<dynamic> values = snapshot.value as List<dynamic>;
-
-          List<Map<String, dynamic>> carts = [];
-          values.forEach((value) {
-            if (value is Map) {
-              carts.add(Map<String, dynamic>.from(value));
-            }
+      if(snapshot.value != null){
+        Map<String,dynamic> cartData = Map<String, dynamic>.from(snapshot.value as Map<Object?, Object?>);
+        cartData.forEach((key, value) {
+          cart.add({
+            'id': key,
+            'name': value['name'],
+            'price': value['price'],
+            'quantity': value['quantity'],
+            'colors': value['colors'],
+            'sizes': value['sizes'],
+            'productId': value['productId'],
+            'images': value['images'],
+            'userId': value['userId'],
+            
           });
-          return carts;
-        } catch (e) {
-          throw Exception('Error ${e}');
-        }
-      } else if (snapshot.value is Map) {
-        try{
-          Map<dynamic, dynamic> value = snapshot.value as Map<dynamic, dynamic>;
-          //print(snapshot.value);
-          return [Map<String, dynamic>.from(value)];
-        }
-        catch(e){
-          throw Exception('Error ${e}');
-        }
-       
-      } else {
-        throw Exception('Formato de dados não reconhecido');
+        });
       }
-    } catch (e) {
-      throw Exception('$e');
+
+      return cart;
+
     }
+    catch(e){throw Exception('Error: ${e}');}
   }
 
   Future<void> store(Map<String, dynamic>? product) async {
@@ -83,9 +71,10 @@ class CartService {
       await databaseReference
           .child('cart')
           .child(_authUser!.uid)
-          .child(product!['id'].toString())
+         // .child(product!['id'].toString())
+          .push()
           .set({
-        'productId': product['id'],
+        'productId': product!['id'],
         'userId': _authUser.uid,
         'user_name': _authUser.displayName,
         'name': product['name'],
