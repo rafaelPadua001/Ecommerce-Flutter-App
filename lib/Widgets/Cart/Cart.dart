54caa879401234f73f6ce04fd848546e1793f3ab
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-05-22 23:48:42 -0300

    Fix
    Remove ScaffoldMessage .

--- lib/Widgets/Cart/Cart.dart
+++ lib/Widgets/Cart/Cart.dart
@@ -93,7 +93,7 @@ class _CartState extends State<Cart> {
     try{
       final saveQuantity = await cartService.updateQuantity(productId.toString(), newQuantity, cartId);
       setState(() {
-         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Quantidade salva $newQuantity')));
+        //  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Quantidade salva $newQuantity')));
         _buildSumPrice();
       });
      
